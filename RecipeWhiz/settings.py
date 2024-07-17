26af7667fc51ce0cc58e9bdfbb0cf9195ef9dc5b
Parsa <parsatonayi5@gmail.com>
2024-07-17 13:08:02 +0330

    Updated

--- RecipeWhiz/settings.py
+++ RecipeWhiz/settings.py
@@ -93,8 +93,8 @@ STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_DIRS =  [BASE_DIR / 'static']
 
-# MEDIA_URL = '/images/'
-# MEDIA_ROOT = BASE_DIR / 'static/images'
+MEDIA_URL = '/images/'
+MEDIA_ROOT = BASE_DIR / 'static/images'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
--- RecipeWhiz/urls.py
+++ RecipeWhiz/urls.py
@@ -2,15 +2,15 @@ from django.contrib import admin
 from django.contrib.auth import views as auth_views
 from django.conf import settings
 from django.conf.urls.static import static
-from users import views as user_views
+from users import views
 from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('login/', auth_views.LoginView.as_view(template_name='accounts/login.html'), name='login'),
-    path('register/', user_views.register, name = 'register'),
-    path('profile/', user_views.profile, name = 'profile'),
-    path('logout/', user_views.logout_view, name='logout'),
+    path('register/', views.register, name = 'register'),
+    path('profile/', views.profile, name = 'profile'),
+    path('logout/', views.logout_view, name='logout'),
     path('', include('recipe.urls')),
 ]
 
